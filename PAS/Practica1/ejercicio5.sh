1e6fd60bc26381f327526bd4da6263b1d9cbda6f
i32petoj <joseperezparras95@gmail.com>
2015-03-07 22:11:05 +0100

    El ejercicio 5 mas chustero del mundo returns

--- PAS/Practica1/ejercicio5.sh
+++ PAS/Practica1/ejercicio5.sh
@@ -2,9 +2,15 @@
 if [ $# -eq 2 ]
 	then
 	
-	find "$1" -name "*$2*"
-
-
+	for x in $(find "$1" -name "*$2*")
+		do
+				if [ -x $x ] #Con esto sabemos si es ejecutable
+					then
+						echo "$x"";"$(stat -c %s $x)";"$(stat -c %i $x)";"$(stat -c %A $x)";1;"
+					else 
+						echo "$x"";"$(stat -c %s $x)";"$(stat -c %i $x)";"$(stat -c %A $x)";0;"
+				fi
+		done
 	else
 		echo "Numero de parametros incorrectos"
 fi
