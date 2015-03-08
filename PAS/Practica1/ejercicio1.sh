c52b2e9d94ef1830fde9e23e508d2d839a5b9449
i32petoj <joseperezparras95@gmail.com>
2015-03-08 13:33:11 +0100

    Chusterín chusterán la magia hará saltar :>

--- PAS/Practica1/ejercicio1.sh
+++ PAS/Practica1/ejercicio1.sh
@@ -1,2 +1,55 @@
 #!/bin/bash
+function busqueda()
+{
+		for x in $(find $1)
+		do
+			if [ -d $x ] #Comprobamos si es una subcarpeta
+			then
+					mkdir "$2/$x"
+			else
+					var=$[$(date +%s)-$(stat $x -c %Y)]
+					if [ $var -gt 20 ]
+					then
+						cp $x "$2/$x"
+					fi
+			fi
+		done
+}
+
+#Comienzo del programa
+if [ $# -lt 1 ] || [ $# -gt 2 ]
+	then
+	echo "Numero de parametros introducidos incorrectos"
+
+	else
+		if [ -d $1 ] #Comprobamos que la carpeta proporcionada existe
+		then
+		#Creamos una carpeta temporal para almacenar los ficheros
+		temp="temporal.tmp"
+		mkdir "$temp"
+
+		#Ahora recorremos las carpetas buscando los ficheros que almacenaremos en la temporal
+		
+		busqueda $1 $temp
+
+		#Y ahora comprimimos
+
+		if [ $# -eq 1 ] #No nos ha ntroducido el nombre del tar
+			then
+				cd "$temp"
+				tar -czvf "../$1.tar.gz" "$1"
+				cd ..
+			else
+				cd "$temp"
+				tar -czvf "../$2" "$1"
+				cd ..
+		fi
+			rm -R "$temp"
+		else
+			echo "Carpeta no valida"
+	fi
+
+fi
+
+
 
--- PAS/Practica1/ejercicio4.sh
+++ PAS/Practica1/ejercicio4.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 if [  $# -eq 2 ]
 	then
-		
-		grep $2 $1 --color -n
+		num=0
+		for x in $(grep $2 $1 -n | cut -d ":" -f1 ) #Con eso cogemos los numeros de las lineas
+		do
+			#Ahora tenemos que imprimir dichas lineas y una mas y una menos
+			echo "---------------------"
+			echo "Muestra numero: $num"
+			i=0
+			j=0
+			var=0
+			#Para contar las lineas del fichero
+			while read line
+			do
+				let "j++"
+			done < $1
+				while read line
+					do
+						let "i++"
 
+							#Mostramos las linea anterior y posterior
+								if [ $((i-1)) -eq $x -o $i -eq $x -o $((i+1)) -eq $x ]
+									then
+										#Ahora tenemos que comprobar si la linea que queremos leer es la primera
+										if [ $x -eq 1 ]
+											then
+												var=1
+										fi
+										#Y comprobamos si esta en la ultima linea
+										if [ $x -eq $j ]
+											then
+											var=2
+										fi
+
+										#Y ahora mostramos por pantalla dependiendo del caso
+										case $var in
+											0) echo "$line" ;;
+											1) echo "La linea a leer es la primera :<" ;;
+											2) echo "La linea a leer es la ultima :<" ;;
+											*) echo "Algo ha fallado" ;;
+										esac
+								fi
+
+					done < $1
+					let "num++"
+				done
 	else
 	echo "Numero de parametros introducidos incorrecto"
 
