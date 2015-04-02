fd7c8033aef1e566506477b84783a0a067f5d438
i32petoj <joseperezparras95@gmail.com>
2015-04-02 20:29:28 +0200

    Vamos a por twitter!

--- PAS/Practica2/ejercicio3.sh
+++ PAS/Practica2/ejercicio3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 #Tengo que hacerlo con el comando who y el comando date sumando y restando
+echo "Lista de usuarios conectados junto con el numero de horas, minutos y segundos que llevan conectados"
 
+#hora=$(who | sed -n -r 's/([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)/\4/p')
 
+#echo $hora
+who | sed -n -r -e "s/([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)[ ]*([^ ]*)/El usuario \1 lleva $[$(date +%H)-$(date -d \4 +%H)] horas, no se cuantos minutos y $(date +%S) segundos/p" | sort
--- PAS/Practica2/ejercicio4.sh
+++ PAS/Practica2/ejercicio4.sh
@@ -6,5 +6,20 @@ if [ ! $# -eq 2 ]
 	
 	else
 
+	echo "Procesando..."
+	while read line
+	do
+		segundos=$(ping $line -c1 -W $2 | grep -E -o 'time=.* ' | sed -r -e 's/time=//' | sort)
 
+		if [ "$segundos" == "" ]
+			then
+			echo "Tras $2 segundos no respondio la IP $line">> temp.tmp
+			else
+			echo "Tras $segundos responde la IP $line">> temp.tmp
+		fi
+	done < $1
+
+	cat temp.tmp | sort
+
+	rm temp.tmp
 fi 
