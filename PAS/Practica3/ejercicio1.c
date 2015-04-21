fc854f27f3259f8643ad29e8fb9f62cbadc25f93
i32petoj <joseperezparras95@gmail.com>
2015-04-21 09:03:18 +0200

    Empezando los ejercicios

--- PAS/Practica3/ejercicio1.c
+++ PAS/Practica3/ejercicio1.c
@@ -6,5 +6,54 @@
 
 int main(int argc, char ** argv){
 
+	int c;
+	//Declaramos algunas banderas para las variables que no pueden ir juntas
+	int nflag = 0;
+	int uflag = 0;
+	int gflag = 0;
+	int eflag = 0;
+	int sflag = 0;
+
+
+
+
+	while((c = getopt(argc, argv, "n:u:ges") != -1){
+
+
+
+		//Y aqui cogemos la opcion que deseamos
+
+		switch(c){
+
+			case 'n':
+
+				break;
+
+			case 'u':
+
+				break;
+
+
+			case 'g':
+
+				break;
+
+			case 'e':
+
+				break;
+
+			case 's':
+
+
+				break;
+
+			case '?':
+					printf("Argumento invalido o argumento necesita una cadena como complementario :>");
+				break;
+
+		}
+
+	}
+
 
 }
