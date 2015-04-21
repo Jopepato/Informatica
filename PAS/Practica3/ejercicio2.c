265f178c99bf702394483be82488a40f129d412f
i32petoj <joseperezparras95@gmail.com>
2015-04-21 19:29:19 +0200

    Ejercicio 1 compilao

--- PAS/Practica3/ejercicio2.c
+++ PAS/Practica3/ejercicio2.c
@@ -1,11 +1,15 @@
+#include <unistd.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-
+#include <string.h>
+#include <getopt.h>
+#include <grp.h>
 
 
 int main(int argc, char ** argv){
+
+	//En este programa usaremos el getoptlong :>
 
 
 return 0;
