a122d99b98feee4a85ac8a7d76ba278d907fc11e
i32petoj <joseperezparras95@gmail.com>
2015-05-16 22:06:51 +0200

    Empezando el Repaso de las actividades de PAS

--- PAS/Practica1/ejercicio5.sh
+++ PAS/Practica1/ejercicio5.sh
@@ -12,18 +12,18 @@ if [ $# -eq 2 ]
 				fi
 		done
 	else
-		if [ $# -eq 0 ]					#No ha introducido ninguno parametro se buscando todos los ficheros de la carpeta actual
+				if [ $# -eq 0 ]	#No ha introducido ninguno parametro se buscando todos los ficheros de la carpeta actual
 
-			for x in $(find)
-			do
-				if [ -x $x ] #Con esto sabemos si es ejecutable
-					then
-						echo "$x;$(stat -c %s $x);$(stat -c %i $x);$(stat -c %A $x);1;"
-					else
-						echo "$x;$(stat -c %s $x);$(stat -c %i $x);$(stat -c %A $x);0;"
+					for x in $(find)
+					do
+						if [ -x $x ] #Con esto sabemos si es ejecutable
+						then
+							echo "$x;$(stat -c %s $x);$(stat -c %i $x);$(stat -c %A $x);1;"
+						else
+							echo "$x;$(stat -c %s $x);$(stat -c %i $x);$(stat -c %A $x);0;"
+						fi
+					done
 				fi
-			done
-		fi
 
 		#Comprobacion de parametros
 
