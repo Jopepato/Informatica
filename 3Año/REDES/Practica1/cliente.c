b17e550d9ba317226bd65366da2291321d37dbb4
i32petoj <joseperezparras95@gmail.com>
2015-09-24 19:19:40 +0200

    Empezando redes

---
+++ 3Año/REDES/Practica1/cliente.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sockets.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <linux/in.h>
+//Includes de las librerias para que funcione el cliente servidor
---
+++ 3Año/REDES/Practica1/servidor.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sockets.h>
+#include <netinet/in.h>
+#include <netdb.h>
