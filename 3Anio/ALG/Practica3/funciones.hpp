74960f918078418b1ccf62d43924fec4c2af8e54
i32petoj <i32petoj@uco.es>
2015-10-29 13:57:32 +0100

    Fusion

--- 3Anio/ALG/Practica3/funciones.hpp
+++ 3Anio/ALG/Practica3/funciones.hpp
@@ -29,25 +29,6 @@ void muestraVector(const vector<T> &v){
 }
 
 template <class T>
-bool maximoMinimoRecur(const vector<T> &v, int &max, int &min, int &llamadas, int i){
-	if(i==v.size()){
-		return true;
-	}else{
-		if(v[i]>max){
-			max = v[i];
-		}
-		if(v[i]<min){
-			min = v[i];
-		}
-		llamadas++;
-		
-		maximoMinimoRecur(v, max, min, llamadas, i+1);
-	}
-	return false;
-}
-
-
-template <class T>
 T Minimo(T min1, T min2){
 	if(min1<min2){
 		return min1;
@@ -70,12 +51,10 @@ template <class T>
 void minimoMaximo(vector<T> &v, int n, int i, int j, int &max, int &min, int &llamadas){
 	int mitad;
 	T maximo1, minimo1, maximo2, minimo2;
+	
 	if(i==j){
 		max = v[i];
 		min = v[i];
-		cout << "Caso base" << endl;
-		cout << "max "<< max<<endl;
-		cout <<" min "<< min <<endl;
 	}else{
 		if(i ==(j-1)){
 			if(v[i]<v[j]){
@@ -85,10 +64,6 @@ void minimoMaximo(vector<T> &v, int n, int i, int j, int &max, int &min, int &ll
 				max = v[i];
 				min = v[j];
 			}
-
-		cout << "Caso genereal" << endl;
-		cout << "max "<< max<<endl;
-		cout <<" min "<< min <<endl;
 		}else{
 			mitad = (i+j)/2;
 			llamadas++;
@@ -98,10 +73,50 @@ void minimoMaximo(vector<T> &v, int n, int i, int j, int &max, int &min, int &ll
 			//muestraVector(v);
 			max = Maximo(maximo1, maximo2);
 			min = Minimo(minimo1, minimo2);
-
 		}
 	}
 }
 
+
+template <class T>
+bool estaOrdenado(vector<T> &v){
+  for(unsigned int i=0; i<v.size()-1; i++){
+    assert(v[i]<=v[i+1]);
+  }
+  return(true);
+}
+
+template <class T>
+void ordenacionBurbuja(vector <T> &v){
+  int aux;
+  int n = v.size();
+  for(int i=0; i<=n; i++){
+    for(int j=0; j<n; j++){
+      if(v[j]>v[j+1]){
+        aux = v[j];
+        v[j] = v[j+1];
+        v[j+1] = aux;
+      }//finsi
+    }//finpara J
+  }//finpara I
+  estaOrdenado(v);
+}
+
+template <class T>
+void fusionar(vector<T> &u, vector<T> &v, vector<T> &t){
+	int i=0, j=0;
+	u[u.size()-1] = INFINITY;
+	v[v.size()-1] = INFINITY;
+	for(int k=0; k<(u.size()+v.size()); k++){
+		if(u[i]<v[j]){
+			t[k] = u[i];
+			i++;
+		}else{
+			t[k] = v[j];
+			j++;
+		}
+	}
+	//Ya se crearia el vector ordenado de la fusion
+}
 
 #endif
---
+++ 3Anio/ALG/Practica3/main2.cpp
@@ -0,0 +1,13 @@
+#include "funciones.hpp"
+
+
+int main(){
+	int maximo;
+	int minimo;
+	int nEle;
+	int llamadas=0;
+	vector<int> v;
+
+	//Ordenacion por fusion
+	return 0;
+}
