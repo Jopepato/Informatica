2449a4b7aa3dc928a2e712572ecb630b721565e6
i32petoj <i32petoj@uco.es>
2016-01-02 20:57:00 +0100

    Parte 4 terminada

--- 3Anio/Web/prueba1/polls/views.py
+++ 3Anio/Web/prueba1/polls/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.template import RequestContext,loader
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 from .models import Question, Choice
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
 
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+    	"""Return the last five published questions"""
+    	return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {'question': p, 'error_message': "You didn't select a choice."})
+
+	else:
+		selected_choice.votes +=1
+		selected_choice.save()
+		#Y tenemos que mostrar algo por pantalla para hacer un render y que asi el usuario no haga lo de volver atras
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
