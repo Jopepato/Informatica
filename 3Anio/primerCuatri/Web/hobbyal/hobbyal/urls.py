5eb8eab8e18594f9814e340407f89085a54cda39
i32petoj <i32petoj@uco.es>
2016-02-23 19:52:27 +0100

    Organizando un poquito

--- 3Anio/primerCuatri/Web/hobbyal/hobbyal/urls.py
+++ 3Anio/primerCuatri/Web/hobbyal/hobbyal/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'hobbyal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'hobbyal.views.index', name='index'),
+    url(r'^web/', include('web.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
